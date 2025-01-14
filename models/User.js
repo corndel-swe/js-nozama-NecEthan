8712bb1fd8a8810b8872b5508f0a0a79e3f1b145
GLOBAL\ethan.wicks <ethan.wicks@necsws.com>
2025-01-14 10:05:31 +0000

    add create user and login api

--- models/User.js
+++ models/User.js
@@ -19,6 +19,65 @@ class User {
 
     return user
   }
+
+  static async addUser(user) {
+
+    const fields = ['username', 'firstName', 'lastName', 'email', 'avatar', 'password'];
+
+    for (let field of fields) {
+      if (!user[field]) {
+        throw new Error('missing property')
+      }
+    }
+
+    const query = `
+    INSERT INTO users (username, firstName, lastName, email, avatar, password)
+    VALUES (?, ?, ?, ?, ?, ?)
+    `;
+
+    const results = await db.raw(query, [
+      user.username,
+      user.firstName,
+      user.lastName,
+      user.email,
+      user.avatar,
+      user.password]);
+    
+    delete results.password
+    return results;
+  }
+
+  static async loginUser(username, password) {
+
+    const query = `SELECT * from users WHERE username = ? AND password = ?`;
+
+    const results = await db.raw(query, [
+      username,
+      password
+    ])
+
+    if (results.length === 0) {
+      return null;
+    }
+
+    delete results[0].password
+    return results;
+  }
+
+  static async deleteUser(userId) {
+    const checkQuery = `SELECT * FROM users WHERE id = ?`;
+    const checkResults = await db.raw(checkQuery, [userId]);
+  
+    if (checkResults.length === 0) {
+      return null;
+    }
+
+    const query = `DELETE FROM users WHERE id = ?`;
+
+    await db.raw(query, [userId])
+
+    return true;
+  }
 }
 
 export default User
--- server/app.js
+++ server/app.js
@@ -1,13 +1,35 @@
 import express from 'express'
-
+import User from '../models/User.js'
 const app = express()
 app.use(express.json())
 
-// You can delete this endpoint
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to Nozama!', time: Date.now() })
-})
+app.post('/users', async (req, res) => {
+  try {
+    const result = await User.addUser(req.body);
+    res.status(200).json({ result});
+  } catch (error) {
+    res.status(400).json({ msg: error.message });
+  }
+});
 
-// TODO: add endpoints during the workshop
+
+app.post('/users/login', async (req, res) => {
+  const result = await User.loginUser(req.body.username, req.body.password)
+  if (result) {
+    res.status(200).json({ result })
+  } else {
+    res.status(404).json({error: "user does not exist"})
+  }
+})
+app.delete('/users/:userId', async (req, res) => {
+    const userId = req.params.userId;
+    const result = await User.deleteUser(userId);
+
+    if (result) {
+      res.status(200).json({ msg: 'User successfully deleted' });
+    } else {
+      res.status(404).json({ error: 'User not found' });
+    }
+});
 
 export default app
